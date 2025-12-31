2865ea894f39f319ac30ba4411df5f526102866c
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-12-31 12:19:52 -0300

    fiz os desafios 2 e 3

---
+++ desafios/desafio03.py
@@ -0,0 +1,15 @@
+print('===== DESAFIO 03 =====')
+
+# o valor retornado é string
+numero1 = input('Primeiro número: ') 
+numero2 = input('Segundo número: ')
+
+# faz a conversão para inteiro
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+# a soma é permitida
+soma = numero1 + numero2
+
+# exibe corretamente
+print('A soma é: ', soma)
